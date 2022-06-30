2dc299eae8a8463418c5ab9210a35322c1eba83f
leandremoritz <leandremoritz19@gmail.com>
2022-06-30 15:57:49 +0200

    update

--- admin.js
+++ admin.js
@@ -3,8 +3,8 @@ let listings = JSON.parse(localStorage.getItem("listings"))
     {image: 'https://raw.githubusercontent.com/leandremoritz/JSproject/main/images/apartment1%20(1).jpg',
     id:1,
      title:'Light and Modern Apartment',
-     price: '$4500/mo',
-     address:'2436 SW 8th St, Miami, FL 33135, USA',
+     price: '4500',
+     address:'Miami',
      bedrooms:'4',
      bathrooms:'2',
      garage:'1',
@@ -17,12 +17,12 @@ let listings = JSON.parse(localStorage.getItem("listings"))
     {image:'https://raw.githubusercontent.com/leandremoritz/JSproject/main/images/apartment1%20(2).jpg',
     id:2,
     title:'Cute Apartment',
-    price: '$4500/mo',
-    address:'2436 SW 8th St, Miami, FL 33135, USA',
-    bedrooms:'4',
-    bathrooms:'2',
+    price: '3500',
+    address:'Los Angeles',
+    bedrooms:'3',
+    bathrooms:'1',
     garage:'1',
-    size:'1200',
+    size:'1000',
     type:'Apartment',
     user:' Samuel Palmer',
     datePosted:'04/04/2020',
@@ -31,8 +31,8 @@ let listings = JSON.parse(localStorage.getItem("listings"))
    {image: 'https://raw.githubusercontent.com/leandremoritz/JSproject/main/images/apartment1%20(3).jpg',
    id:3,
    title:'Cozy Apartment',
-   price: '$4500/mo',
-   address:'2436 SW 8th St, Miami, FL 33135, USA',
+   price: '3550',
+   address:'Chicago',
    bedrooms:'4',
    bathrooms:'2',
    garage:'1',
@@ -45,12 +45,12 @@ let listings = JSON.parse(localStorage.getItem("listings"))
   {image: 'https://raw.githubusercontent.com/leandremoritz/JSproject/main/images/apartment1%20(4).jpg',
   id:4,
   title:'Newly Built Apartment',
-  price: '$4500/mo',
-  address:'2436 SW 8th St, Miami, FL 33135, USA',
+  price: '2050',
+  address:'New York',
   bedrooms:'4',
   bathrooms:'2',
   garage:'1',
-  size:'1200',
+  size:'1100',
   type:'Apartment',
   user:' Samuel Palmer',
   datePosted:'04/04/2020',
@@ -59,12 +59,12 @@ let listings = JSON.parse(localStorage.getItem("listings"))
  {image: 'https://raw.githubusercontent.com/leandremoritz/JSproject/main/images/apartment1%20(5).jpg',
      id:5,
  title:'Fully Furnished Apartment',
- price: '$4500/mo',
- address:'2436 SW 8th St, Miami, FL 33135, USA',
- bedrooms:'4',
- bathrooms:'2',
+ price: '4000',
+ address:'Miami',
+ bedrooms:'2',
+ bathrooms:'1',
  garage:'1',
- size:'1200',
+ size:'1000',
  type:'Apartment',
  user:' Samuel Palmer',
  datePosted:'04/04/2020',
@@ -73,13 +73,13 @@ let listings = JSON.parse(localStorage.getItem("listings"))
 {image: 'https://raw.githubusercontent.com/leandremoritz/JSproject/main/images/house1%20(1).jpg',
     id:6,
      title:'North Facing listing',
-     price: '$4500/mo',
-     address:'2436 SW 8th St, Miami, FL 33135, USA',
+     price: '2M',
+     address:'New York',
      bedrooms:'4',
      bathrooms:'2',
-     garage:'1',
-     size:'1200',
-     type:'Apartment',
+     garage:'2',
+     size:'2000',
+     type:'House',
      user:' Samuel Palmer',
      datePosted:'04/04/2020',
      yearBuilt:'2016',
@@ -87,13 +87,13 @@ let listings = JSON.parse(localStorage.getItem("listings"))
     {image: 'https://raw.githubusercontent.com/leandremoritz/JSproject/main/images/house1%20(2).jpg',
         id:7,
     title:'Newly Built listing',
-    price: '$4500/mo',
-    address:'2436 SW 8th St, Miami, FL 33135, USA',
+    price: '1.5M',
+    address:"Los Angeles",
     bedrooms:'4',
     bathrooms:'2',
-    garage:'1',
-    size:'1200',
-    type:'Apartment',
+    garage:'3',
+    size:'2200',
+    type:'House',
     user:' Samuel Palmer',
     datePosted:'04/04/2020',
     yearBuilt:'2016',
@@ -101,13 +101,13 @@ let listings = JSON.parse(localStorage.getItem("listings"))
    {image: 'https://raw.githubusercontent.com/leandremoritz/JSproject/main/images/studio%20(1).jpg',
        id:8,
    title:'Grand Mansion',
-   price: '$4500/mo',
-   address:'2436 SW 8th St, Miami, FL 33135, USA',
-   bedrooms:'4',
-   bathrooms:'2',
-   garage:'1',
-   size:'1200',
-   type:'Apartment',
+   price: '1000',
+   address:'Miami',
+   bedrooms:'0',
+   bathrooms:'1',
+   garage:'2',
+   size:'900',
+   type:'Studio',
    user:' Samuel Palmer',
    datePosted:'04/04/2020',
    yearBuilt:'2016',
@@ -115,13 +115,13 @@ let listings = JSON.parse(localStorage.getItem("listings"))
   {image: 'https://raw.githubusercontent.com/leandremoritz/JSproject/main/images/studio%20(2).jpg',
   id:9,
   title:'Bachelor Pad',
-  price: '$4500/mo',
-  address:'2436 SW 8th St, Miami, FL 33135, USA',
-  bedrooms:'4',
+  price: '2500',
+  address:'Chicago',
+  bedrooms:'1',
   bathrooms:'2',
-  garage:'1',
-  size:'1200',
-  type:'Apartment',
+  garage:'5',
+  size:'1000',
+  type:'Studio',
   user:' Samuel Palmer',
   datePosted:'04/04/2020',
   yearBuilt:'2016',
@@ -129,13 +129,13 @@ let listings = JSON.parse(localStorage.getItem("listings"))
  {image: 'https://raw.githubusercontent.com/leandremoritz/JSproject/main/images/studio%20(3).jpg',
  id:10,
  title:'Pentlisting Open Floor',
- price: '$4500/mo',
- address:'2436 SW 8th St, Miami, FL 33135, USA',
- bedrooms:'4',
+ price: '2000',
+ address:'New York',
+ bedrooms:'1',
  bathrooms:'2',
- garage:'1',
- size:'1200',
- type:'Apartment',
+ garage:'4',
+ size:'1100',
+ type:'Studio',
  user:' Samuel Palmer',
  datePosted:'04/04/2020',
  yearBuilt:'2016',
@@ -147,7 +147,7 @@ function ShowlistingListings (listings) {
       propertiesTable.innerHTML += `
         <tr>
         <td>${listing.id}</td>
-          <td><img src=${listing.image}</td>
+          <td><img src=${listing.image} /></td>
          
           <td>${listing.title}</td>
           <td>${listing.address}</td>
@@ -182,7 +182,64 @@ function ShowlistingListings (listings) {
     listing.title = input; //Changes the content to what the user's input
     localStorage.setItem("listings", JSON.stringify(listings));
     ShowlistingListings(listings); //Displays the array
+  };
+
+  
+function typeHouse() {
+    let value = document.getElementById("select1").value;
+    let filListings = listings.filter((listing) => {
+      return listing.type === value     //loop through houses and return houses which id noes not match
+    });
+    ShowlistingListings(filListings);
   }
+  function addressHouse() {
+    let value = document.getElementById("select2").value;
+    let filListings = listings.filter((listing) => {
+      return listing.address === value     //loop through houses and return houses which id noes not match
+    });
+    ShowlistingListings(filListings);
+  }
+
+  function bedroomsHouse() {
+    let value = document.getElementById("select3").value;
+    let filListings = listings.filter((listing) => {
+      return listing.bedrooms === value     //loop through houses and return houses which id noes not match
+    });
+    ShowlistingListings(filListings);
+  }
+   
+  function sortListings(){
+    
+    let price = listings.sort((a, b) => {
+      return a.price - b.price;
+    });
+    let retrievedList = price;
+    document.querySelector("#listingsbox").innerHTML = "";
+    retrievedList.forEach((propperty) => {
+      ShowlistingListings(retrievedList);
+  })
+}
+function addListing() {
+  let property = {
+    id: listings.length + 1,
+    title: document.querySelector("#Title").value,
+    area: document.querySelector("#Location").value,
+    image: document.querySelector("#Image").value,
+    type: document.querySelector("#Type").value,
+    bedrooms: document.querySelector("#Bedrooms").value,
+    baths: document.querySelector("#Bathrooms").value,
+    garage: document.querySelector("#Garage").value,
+    size: document.querySelector("#Size").value,
+    price: document.querySelector("#Price").value,
+    status: document.querySelector("#Status").value,
+  };
+  listings.push(property);
+  localStorage.setItem("properties", JSON.stringify(listings));
+  ShowlistingListings(listings);
+ 
+}
+
+
  
 
 
--- script.js
+++ script.js
@@ -3,8 +3,8 @@ let listings = JSON.parse(localStorage.getItem("listings"))
     {image: 'https://raw.githubusercontent.com/leandremoritz/JSproject/main/images/apartment1%20(1).jpg',
     id:1,
      title:'Light and Modern Apartment',
-     price: '$4500/mo',
-     address:'2436 SW 8th St, Miami, FL 33135, USA',
+     price: '4500',
+     address:'Miami',
      bedrooms:'4',
      bathrooms:'2',
      garage:'1',
@@ -17,12 +17,12 @@ let listings = JSON.parse(localStorage.getItem("listings"))
     {image:'https://raw.githubusercontent.com/leandremoritz/JSproject/main/images/apartment1%20(2).jpg',
     id:2,
     title:'Cute Apartment',
-    price: '$4500/mo',
-    address:'2436 SW 8th St, Miami, FL 33135, USA',
-    bedrooms:'4',
-    bathrooms:'2',
+    price: '3500',
+    address:'Los Angeles',
+    bedrooms:'3',
+    bathrooms:'1',
     garage:'1',
-    size:'1200',
+    size:'1000',
     type:'Apartment',
     user:' Samuel Palmer',
     datePosted:'04/04/2020',
@@ -31,8 +31,8 @@ let listings = JSON.parse(localStorage.getItem("listings"))
    {image: 'https://raw.githubusercontent.com/leandremoritz/JSproject/main/images/apartment1%20(3).jpg',
    id:3,
    title:'Cozy Apartment',
-   price: '$4500/mo',
-   address:'2436 SW 8th St, Miami, FL 33135, USA',
+   price: '3550',
+   address:'Chicago',
    bedrooms:'4',
    bathrooms:'2',
    garage:'1',
@@ -45,12 +45,12 @@ let listings = JSON.parse(localStorage.getItem("listings"))
   {image: 'https://raw.githubusercontent.com/leandremoritz/JSproject/main/images/apartment1%20(4).jpg',
   id:4,
   title:'Newly Built Apartment',
-  price: '$4500/mo',
-  address:'2436 SW 8th St, Miami, FL 33135, USA',
+  price: '2050',
+  address:'New York',
   bedrooms:'4',
   bathrooms:'2',
   garage:'1',
-  size:'1200',
+  size:'1100',
   type:'Apartment',
   user:' Samuel Palmer',
   datePosted:'04/04/2020',
@@ -59,12 +59,12 @@ let listings = JSON.parse(localStorage.getItem("listings"))
  {image: 'https://raw.githubusercontent.com/leandremoritz/JSproject/main/images/apartment1%20(5).jpg',
      id:5,
  title:'Fully Furnished Apartment',
- price: '$4500/mo',
- address:'2436 SW 8th St, Miami, FL 33135, USA',
- bedrooms:'4',
- bathrooms:'2',
+ price: '4000',
+ address:'Miami',
+ bedrooms:'2',
+ bathrooms:'1',
  garage:'1',
- size:'1200',
+ size:'1000',
  type:'Apartment',
  user:' Samuel Palmer',
  datePosted:'04/04/2020',
@@ -73,13 +73,13 @@ let listings = JSON.parse(localStorage.getItem("listings"))
 {image: 'https://raw.githubusercontent.com/leandremoritz/JSproject/main/images/house1%20(1).jpg',
     id:6,
      title:'North Facing listing',
-     price: '$4500/mo',
-     address:'2436 SW 8th St, Miami, FL 33135, USA',
+     price: '2M',
+     address:'New York',
      bedrooms:'4',
      bathrooms:'2',
-     garage:'1',
-     size:'1200',
-     type:'Apartment',
+     garage:'2',
+     size:'2000',
+     type:'House',
      user:' Samuel Palmer',
      datePosted:'04/04/2020',
      yearBuilt:'2016',
@@ -87,13 +87,13 @@ let listings = JSON.parse(localStorage.getItem("listings"))
     {image: 'https://raw.githubusercontent.com/leandremoritz/JSproject/main/images/house1%20(2).jpg',
         id:7,
     title:'Newly Built listing',
-    price: '$4500/mo',
-    address:'2436 SW 8th St, Miami, FL 33135, USA',
+    price: '1.5M',
+    address:"Los Angeles",
     bedrooms:'4',
     bathrooms:'2',
-    garage:'1',
-    size:'1200',
-    type:'Apartment',
+    garage:'3',
+    size:'2200',
+    type:'House',
     user:' Samuel Palmer',
     datePosted:'04/04/2020',
     yearBuilt:'2016',
@@ -101,13 +101,13 @@ let listings = JSON.parse(localStorage.getItem("listings"))
    {image: 'https://raw.githubusercontent.com/leandremoritz/JSproject/main/images/studio%20(1).jpg',
        id:8,
    title:'Grand Mansion',
-   price: '$4500/mo',
-   address:'2436 SW 8th St, Miami, FL 33135, USA',
-   bedrooms:'4',
-   bathrooms:'2',
-   garage:'1',
-   size:'1200',
-   type:'Apartment',
+   price: '1000',
+   address:'Miami',
+   bedrooms:'0',
+   bathrooms:'1',
+   garage:'2',
+   size:'900',
+   type:'Studio',
    user:' Samuel Palmer',
    datePosted:'04/04/2020',
    yearBuilt:'2016',
@@ -115,13 +115,13 @@ let listings = JSON.parse(localStorage.getItem("listings"))
   {image: 'https://raw.githubusercontent.com/leandremoritz/JSproject/main/images/studio%20(2).jpg',
   id:9,
   title:'Bachelor Pad',
-  price: '$4500/mo',
-  address:'2436 SW 8th St, Miami, FL 33135, USA',
-  bedrooms:'4',
+  price: '2500',
+  address:'Chicago',
+  bedrooms:'1',
   bathrooms:'2',
-  garage:'1',
-  size:'1200',
-  type:'Apartment',
+  garage:'5',
+  size:'1000',
+  type:'Studio',
   user:' Samuel Palmer',
   datePosted:'04/04/2020',
   yearBuilt:'2016',
@@ -129,13 +129,13 @@ let listings = JSON.parse(localStorage.getItem("listings"))
  {image: 'https://raw.githubusercontent.com/leandremoritz/JSproject/main/images/studio%20(3).jpg',
  id:10,
  title:'Pentlisting Open Floor',
- price: '$4500/mo',
- address:'2436 SW 8th St, Miami, FL 33135, USA',
- bedrooms:'4',
+ price: '2000',
+ address:'New York',
+ bedrooms:'1',
  bathrooms:'2',
- garage:'1',
- size:'1200',
- type:'Apartment',
+ garage:'4',
+ size:'1100',
+ type:'Studio',
  user:' Samuel Palmer',
  datePosted:'04/04/2020',
  yearBuilt:'2016',
@@ -150,18 +150,57 @@ function showlistings (listings) {
     listings.forEach(listing => {
         propertiesContainer.innerHTML += `
       
-        </section>
             <div id='listingcards'>
                 <img src=${ listing.image } />
                  <h3>${listing.title}</h3>
                  <p>${listing.address}</p>
+                 <i class="fa-solid fa-dollar-sign"></i>${listing.price}</li>
                  <ul id=listing-values>
                  <li><i class="fa-solid fa-bed"></i>${listing.bedrooms}</li>
                  <li><i class="fa-solid fa-shower"></i>${listing.bathrooms}</li>
                 <li><i class="fa-solid fa-car"></i>${listing.garage}</li>
                 <li><i class="fa-solid fa-ruler"></i>${listing.size}</li>
+                
             </div>
         `
     })
 }
 showlistings(listings);
+
+
+function typeHouse() {
+    let value = document.getElementById("select1").value;
+    let filListings = listings.filter((listing) => {
+      return listing.type === value     //loop through houses and return houses which id noes not match
+    });
+    showlistings(filListings);
+  }
+  function addressHouse() {
+    let value = document.getElementById("select2").value;
+    let filListings = listings.filter((listing) => {
+      return listing.address === value     //loop through houses and return houses which id noes not match
+    });
+    showlistings(filListings);
+  }
+
+  function bedroomsHouse() {
+    let value = document.getElementById("select3").value;
+    let filListings = listings.filter((listing) => {
+      return listing.bedrooms === value     //loop through houses and return houses which id noes not match
+    });
+    showlistings(filListings);
+  }
+  
+  
+  function sortListings(){
+    
+      let price = listings.sort((a, b) => {
+        return a.price - b.price;
+      });
+      let retrievedList = price;
+      document.querySelector("#properties").innerHTML = "";
+      retrievedList.forEach((propperty) => {
+      showlistings(retrievedList)
+    })
+}
+
